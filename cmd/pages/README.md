d6f4a62dd0164aba4350dd5ad0edde1990e1f8c7
Miek Gieben <miek@miek.nl>
2022-02-22 14:21:56 +0100

    Make it run in parallel
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ cmd/pages/README.md
@@ -0,0 +1,19 @@
+# Pages
+
+Pages will take a file containing Go repositories links (one per line), and a directory path.
+
+It will:
+
+1. download (--depth 1) each repo
+2. run gogo2doc on the repo
+3. capture the result in /path/path/to/repo/README.md
+4. delete the repo
+
+Then as a separate step the README.md can be converted to HTML.
+
+## Repos
+
+Each repo path should be an URL that contains the Go code, this should also be the import path.
+
+Optionally you can specify a git branch seperated by white space on the same line as well. If not
+given it defaults to 'master'.
---
+++ cmd/pages/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"log"
+	"net/url"
+	"os"
+	"os/exec"
+	"path"
+	"sync"
+
+	"github.com/miekg/godoc2md"
+)
+
+func main() {
+	flag.Parse()
+	if len(flag.Args()) != 2 {
+		log.Fatal("Need at least a file to read from and a directory to output to")
+	}
+	repof, err := os.ReadFile(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := mkdirAll(flag.Arg(1)); err != nil {
+		log.Fatal(err)
+	}
+
+	repos := bytes.Split(repof, []byte{'\n'})
+
+	var wg sync.WaitGroup
+	sem := make(chan int, 5) // make 5 an option
+	for i, r := range repos {
+		if len(r) == 0 { // last line
+			continue
+		}
+		log.Printf("Looking at %d, %s", i, r)
+
+		branch := "master"
+		rs := bytes.Fields(r)
+		repo := string(rs[0])
+		if len(rs) == 2 {
+			branch = string(rs[1])
+		}
+
+		wg.Add(1)
+		sem <- 1
+		go func() {
+			defer func() { <-sem; wg.Done() }()
+
+			buf, err := clone(repo, branch)
+			if err != nil {
+				log.Printf("Not good: %v", err)
+				return
+			}
+			url, _ := url.Parse(repo) // parsed in clone() as well
+			dir := path.Join(flag.Arg(1), path.Join(url.Host, url.Path))
+			out := path.Join(dir, "README.md")
+			if err := mkdirAll(dir); err != nil {
+				log.Printf("Not good %s: %v", repo, err)
+				return
+			}
+			log.Printf("Writing to markdown to %s", out)
+			if err := os.WriteFile(out, buf, 0600); err != nil {
+				log.Printf("Not good %s: %v", repo, err)
+				return
+			}
+		}()
+	}
+	wg.Wait()
+
+}
+
+func mkdirAll(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.MkdirAll(path, 0777)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// clone clones the repo and returns the generated config or an error. If err is not nil
+// the returned buffer contains the output of the command that ran.
+func clone(repo, branch string) ([]byte, error) {
+	tmpdir, err := os.MkdirTemp("/tmp", "pages")
+	if err != nil {
+		return nil, err
+	}
+	defer func() { os.RemoveAll(tmpdir) }()
+
+	git := exec.Command("git", "clone", "--depth", "1", "--branch", branch, repo, tmpdir)
+	git.Dir = tmpdir
+
+	out, err := git.CombinedOutput()
+	if err != nil {
+		return out, err
+	}
+
+	url, err := url.Parse(repo)
+	if err != nil {
+		return nil, err
+	}
+	imp := path.Join(url.Host, url.Path)
+	log.Printf("Working on repo %q, with import %q in %q", repo, imp, tmpdir)
+
+	config := &godoc2md.Config{
+		DeclLinks: true,
+		//		Replace:           *flgReplace,
+		Import: imp,
+		GitRef: "main", // from file
+	}
+
+	buf := &bytes.Buffer{}
+	err = godoc2md.Transform(buf, tmpdir, config)
+	return buf.Bytes(), err
+}
