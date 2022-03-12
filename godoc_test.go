951bbc5ea3950388cc4c1fe666990ddfb41e45de
Miek Gieben <miek@miek.nl>
2022-03-12 18:15:23 +0100

    fix test
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- godoc_test.go
+++ godoc_test.go
@@ -9,16 +9,16 @@ import (
 )
 
 func TestURLForFile(t *testing.T) {
-	u := urlForFile("github.com/miekg/dns/scan.go", "github.com/miekg/dns", "main")
+	u := urlForFile("github.com/miekg/dns/scan.go", "github.com/miekg/dns", "main", "")
 	if exp := "https://github.com/miekg/dns/blob/main/scan.go"; u != exp {
 		t.Errorf("expected %s, got %s", exp, u)
 	}
 
-	u = urlForFile("github.com/miekg/gitlabutil/scan.go", "github.com/miekg/gitlabutil", "main")
+	u = urlForFile("github.com/miekg/gitlabutil/scan.go", "github.com/miekg/gitlabutil", "main", "")
 	if exp := "https://github.com/miekg/gitlabutil/blob/main/scan.go"; u != exp {
 		t.Errorf("expected %s, got %s", exp, u)
 	}
-	u = urlForFile("gitlab.com/miekg/dns/scan.go", "gitlab.com/miekg/dns", "main")
+	u = urlForFile("gitlab.com/miekg/dns/scan.go", "gitlab.com/miekg/dns", "main", "")
 	if exp := "https://gitlab.com/miekg/dns/-/blob/main/scan.go"; u != exp {
 		t.Errorf("expected %s, got %s", exp, u)
 	}
